6b18b294984b2e8565cc6a16d84c9e20349daa44
tony <khaihoang2301@gmail.com>
2022-04-08 15:23:00 +0700

    first commit

---
+++ polls/views.py
@@ -0,0 +1,17 @@
+from curses.ascii import HT
+from urllib import response
+from django.http import HttpResponse
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." %question_id)
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." %question_id)
